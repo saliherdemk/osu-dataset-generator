f93f21b181173486ae80b960b308a93ce5830f3e
saliherdemk <saliherdem_kaymak@hotmail.com>
2025-09-18 14:44:59 +0300

    audio divided

--- Tokenizer/divide_into_chunks.py
+++ Tokenizer/divide_into_chunks.py
@@ -1,7 +1,12 @@
 import argparse
+import os
 
+import librosa
 import numpy as np
 import pandas as pd
+import torch
+from tqdm import tqdm
+from transformers import AutoModel, AutoProcessor
 
 
 def get_chunks_for_hit(hit_start_ms, duration_ms, chunk_length_ms, chunk_stride_ms):
@@ -29,24 +34,16 @@ def normalize(df_exploded):
     max_repeat = df_exploded["repeat"].max()
     df_exploded["repeat"] = np.log1p(df_exploded["repeat"]) / np.log1p(max_repeat)
 
-    type_map = {"circle": 0, "slider": 1, "spinner": 2}
-    df_exploded["type"] = df_exploded["type"].map(type_map)
+    type_onehot = pd.get_dummies(df_exploded["type"], prefix="type").astype(int)
+    df_exploded = df_exploded.drop(columns=["type"])
+    df_exploded = pd.concat([df_exploded, type_onehot], axis=1)
+
+    df_exploded["difficulty_rating"] = df_exploded["difficulty_rating"] / 10
 
     return df_exploded
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input_file", required=True)
-    parser.add_argument("--output_file", required=True)
-
-    chunk_length_sec = 10
-    overlap_sec = 5
-
-    args = parser.parse_args()
-
-    df = pd.read_csv(args.input_file)
-
+def divide_tokens(df, chunk_length_sec, overlap_sec):
     chunk_length_ms = chunk_length_sec * 1000
     chunk_stride_ms = (chunk_length_sec - overlap_sec) * 1000
 
@@ -80,14 +77,82 @@ def main():
         "chunk_id",
         "hit_start_rel",
         "hit_end_rel",
+        "difficulty_rating",
     ]
 
     df_exploded = df_exploded[cols]
 
     df_exploded = normalize(df_exploded)
 
-    df_exploded.to_csv(args.output_file, index=False)
-    print(f"Updated file saved to {args.input_file}")
+    return df_exploded
+
+
+def divide_audio(audio_folder, embedding_folder, chunk_length_sec, overlap_sec):
+    target_sr = 24000
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    os.makedirs(embedding_folder, exist_ok=True)
+
+    processor = AutoProcessor.from_pretrained(
+        "m-a-p/MERT-v1-95M", trust_remote_code=True
+    )
+    model = AutoModel.from_pretrained("m-a-p/MERT-v1-95M", trust_remote_code=True)
+    model.eval()
+    model.to(device)
+
+    audio_files = [f for f in os.listdir(audio_folder)]
+
+    for audio_file in tqdm(audio_files, desc="Processing audio"):
+        audio_path = os.path.join(audio_folder, audio_file)
+        beatmap_id = os.path.splitext(audio_file)[0]
+
+        y, sr = librosa.load(audio_path, sr=target_sr)
+        total_sec = y.shape[0] / sr
+        chunk_stride = chunk_length_sec - overlap_sec
+        chunk_starts = np.arange(0, total_sec, chunk_stride)
+
+        for chunk_idx, start_sec in enumerate(chunk_starts):
+            end_sec = start_sec + chunk_length_sec
+            start_sample = int(start_sec * sr)
+            end_sample = int(end_sec * sr)
+            chunk_audio = y[start_sample:end_sample].astype(np.float32)
+            if len(chunk_audio) == 0:
+                continue
+
+            inputs = processor(
+                raw_speech=chunk_audio, sampling_rate=sr, return_tensors="pt"
+            ).to(device)
+            with torch.no_grad():
+                outputs = model(**inputs, output_hidden_states=True)
+
+            all_layer_hidden_states = torch.stack(outputs.hidden_states).squeeze()
+            time_reduced_hidden_states = all_layer_hidden_states.mean(-2)
+
+            save_name = f"{beatmap_id}_chunk{chunk_idx}.pt"
+            save_path = os.path.join(embedding_folder, save_name)
+            torch.save(time_reduced_hidden_states, save_path)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_folder", required=True)
+    parser.add_argument("--output_folder", required=True)
+
+    chunk_length_sec = 10
+    overlap_sec = 5
+
+    args = parser.parse_args()
+
+    formatted_df = pd.read_csv(os.path.join(args.input_folder, "formatted.csv"))
+    chunked_df = divide_tokens(formatted_df, chunk_length_sec, overlap_sec)
+
+    audio_folder = os.path.join(args.input_folder, "audio")
+    embedding_folder = os.path.join(args.output_folder, "audio")
+    os.makedirs(embedding_folder, exist_ok=True)
+
+    divide_audio(audio_folder, embedding_folder, chunk_length_sec, overlap_sec)
+
+    chunked_df.to_csv(os.path.join(args.output_folder, "chunked.csv"), index=False)
 
 
 if __name__ == "__main__":
